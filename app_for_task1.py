04c7e9aaf888bfc69b76da17bdf3544026d4c794
ankkarp <anna.a.k.2002@gmail.com>
2022-09-06 17:07:28 +0300

    changed help for task1 app

--- app_for_task1.py
+++ app_for_task1.py
@@ -2,7 +2,7 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('email', help='Потенциальный имэйл')
+parser.add_argument('email', help='Потенциальный email')
 args = parser.parse_args()
 
 print(bool(re.fullmatch(r"\w+@\w+\.\w+", args.email)))
